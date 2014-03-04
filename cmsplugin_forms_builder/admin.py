fe321fec5329f4193b3976b246e239757c34e4f9
Eric Amador <eric.amador@nimbisservices.com>
2014-03-04 09:00:42 -0500

    Removed unused import.

--- cmsplugin_forms_builder/admin.py
+++ cmsplugin_forms_builder/admin.py
@@ -2,8 +2,6 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from models import PluginForm
 from django.utils.translation import ugettext_lazy as _
-from forms_builder.forms.views import form_detail
-
 
 class FormBuilderPlugin(CMSPluginBase):
     """
@@ -17,6 +15,5 @@ class FormBuilderPlugin(CMSPluginBase):
     def render(self, context, instance, placeholder):
         context['form'] = instance.form
         return context
-
 
 plugin_pool.register_plugin(FormBuilderPlugin)
