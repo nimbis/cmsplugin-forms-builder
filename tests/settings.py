3708960a6ab634daba1337990124691699df64ff
Eric Amador <eric.amador@nimbisservices.com>
2014-03-25 16:44:12 -0400

    Added unit tests.

--- tests/settings.py
+++ tests/settings.py
@@ -115,6 +115,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'menus',
     'mptt',
     'cms',
     'forms_builder.forms',
